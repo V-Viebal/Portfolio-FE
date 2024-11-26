1aefac1e4b87a1bddcdbd3df2b3b7784ce9d1960
mons <dangminh200320@hotmail.com>
2024-11-26 23:19:20 +0700

    feat: update scrolling ssr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, withInMemoryScrolling } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -11,10 +11,16 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [ RouterModule.forRoot( routes, {
-		initialNavigation: 'enabledBlocking',
-		useHash: false
-	} ) ],
+	imports: [
+		RouterModule.forRoot( routes, {
+			initialNavigation: 'enabledBlocking',
+			useHash: false,
+			...withInMemoryScrolling({
+				scrollPositionRestoration: 'enabled',
+				anchorScrolling: 'enabled',
+			}),
+		}),
+	],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
